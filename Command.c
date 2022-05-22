ac9df8cff4e9603b07979f43e30fa204d1a90c30
Pratik Chavan <106006986+PratikChavan19@users.noreply.github.com>
2022-05-22 13:24:50 +0530

    Add files via upload

---
+++ Command.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int ans = 0;
+    printf("Name of executable is : %s\n",argv[0]);
+
+    printf("Number of command line arguments are : %d\n",argc);
+
+    ans = atoi(argv[1]) + atoi(argv[2]);
+    printf("Addition is : %d\n",ans);
+
+    return 0;
+}
